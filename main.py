a334c608e67a26aef8cd836d4cb227a4ff3e30af
Kwiatek909 <testowny1@gmail.com>
2025-06-23 19:44:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,22 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 @bot.event
 async def on_ready():
     print(f'Bot {bot.user} jest gotowy!')
+    
+    # Ustawianie statusu bota
+    await bot.change_presence(
+        status=discord.Status.online,  # online, idle, dnd, invisible
+        activity=discord.Game(name="Pomocny Bot")  # Co bot "gra"
+    )
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('Pong!')
+
+# Komenda do zmiany statusu (opcjonalna)
+@bot.command()
+async def status(ctx, *, new_status):
+    """Zmienia status bota"""
+    await bot.change_presence(activity=discord.Game(name=new_status))
+    await ctx.send(f'Status zmieniony na: {new_status}')
 
 bot.run(os.getenv('DISCORD_TOKEN'))
